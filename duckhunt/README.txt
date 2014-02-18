04df8bac138fd64f542214f32ead057a78a4bedb
Maya Saxena <maya.saxena@tufts.edu>
2014-02-18 16:27:41 -0500

    started work on draw function

---
+++ duckhunt/README.txt
@@ -0,0 +1,10 @@
+/* README.txt
+ * Maya Saxena
+ * COMP20 Assignment 2
+ */
+
+
+Acknowledgements:
+
+- For help with drawImage:
+https://developer.mozilla.org/samples/canvas-tutorial/3_1_canvas_drawimage.html
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,29 @@
-// Your work goes here...
+function draw() {
+
+        canvas = document.getElementById("game");
+        if(canvas.getContext) {
+                ctx = canvas.getContext("2d");
+        } 
+        else {
+                alert("Canvas not supported on browser.");
+        }
+
+        
+
+        var img = new Image();
+
+        img.src = "assets/duckhunt.png";
+
+
+        img.onload = function() {
+                
+                /* To get rid of blurring */
+                ctx.mozImageSmoothingEnabled = false;
+                ctx.webkitImageSmoothingEnabled = false;
+
+                ctx.drawImage(img, 0, 275, 75, 130, 155, 250, 150, 260);
+                ctx.drawImage(img, 0, 500, 900, 500, 0, 200, 900, 500);
+                ctx.drawImage(img, 0, 0, 60, 45, 300, 475, 120, 90);
+        }
+
+}
